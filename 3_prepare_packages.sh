1e7bc27130ff16ca27caf56a98723ddbe9d1f29b
vernesong <42875168+vernesong@users.noreply.github.com>
2020-10-07 07:26:47 +0800

    Update 3_prepare_packages.sh

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -10,6 +10,10 @@ rm -rf luci-theme-infinityfreedom/ && git clone https://github.com/xiaoqingfengA
 rm -rf luci-app-chinadns-ng/ && git clone https://github.com/WuSiYu/luci-app-chinadns-ng
 rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinadns-ng
 
+sed -i '/#include <sfe_backport.h>/c\#include "sfe_backport.h"' "./fast-classifier/src/fast-classifier.c" 2>/dev/null
+sed -i '/#include <sfe.h>/c\#include "sfe.h"' "./fast-classifier/src/fast-classifier.c" 2>/dev/null
+sed -i '/#include <sfe_cm.h>/c\#include "sfe_cm.h"' "./fast-classifier/src/fast-classifier.c" 2>/dev/null
+
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
 #git clone https://github.com/jerrykuku/luci-app-vssr.git
 #cd luci-app-vssr/root/etc/
